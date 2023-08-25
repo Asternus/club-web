e9a7aecfaa752c7c0748ee164f98125f52092509
Asternus <malafiko@gmail.com>
2023-08-25 23:59:31 +0300

    validation

--- src/components/RegistrationUser.vue
+++ src/components/RegistrationUser.vue
@@ -5,22 +5,28 @@
       <div>
         <label for="username">Имя пользователя:</label>
         <input type="text" id="username" v-model="username" required>
+        <div v-if="registrationError.username" class="error-message">
+          {{ registrationError.username}}
+        </div>
       </div>
       <div>
         <label for="email">Email:</label>
         <input type="email" id="email" v-model="email" required>
+        <div v-if="registrationError.email" class="error-message">
+          {{ registrationError.email}}
+        </div>
       </div>
       <div>
         <label for="password">Пароль:</label>
         <input type="password" id="password" v-model="password" required>
+        <div v-if="registrationError.password" class="error-message">
+          {{ registrationError.password}}
+        </div>
       </div>
       <div>
         <button type="submit">Зарегистрироваться</button>
       </div>
     </form>
-    <div v-if="registrationError" class="error-message">
-      {{ registrationError }}
-    </div>
   </div>
 </template>
 
@@ -44,11 +50,12 @@ export default {
           email: this.email,
           password: this.password
         });
+        debugger;
         // Обработка успешной регистрации
         console.log('Пользователь успешно зарегистрирован:', response.data);
       } catch (error) {
         console.error('Ошибка при регистрации пользователя:', error);
-        this.registrationError = 'Ошибка при регистрации пользователя';
+        this.registrationError = error.response.data;
       }
     }
   }
